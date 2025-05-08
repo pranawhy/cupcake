2f7c82dc2c9c8c74fc06ded207869c1e997882a6
pranawhy <pranavpatil7950@gmail.com>
2025-05-08 19:06:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+    lines: [
+        "Dear Chup,",
+        "I still remember the first time we spoke — who knew a person wtih helmet like hair behind a screen could become my entire mood?",
+        "Your curly hair, those pretty eyes, the way you say 'chup'i lovveee itt — it's all stuck with me.",
+        "Sometimes you shout so loud, sometimes you laugh, but even your chaos feels like home to me.",
+        "I wish I could bottle your presence and smash it in your head when your are rude to me — which is almost always.",
+        "Even miles apart, I feel you with me in every quiet moment and every loud call.",
+        "You're not just a voice in my headphones — you're the mood in my life.",
+        "No matter how far we are, you're my always, my 'chup', my favorite kind of madness.",
+        "I love your attention or rudeness,no matter how many times you get mad at me,i'll always do my best.",
+         "— Pranav,To My Cupcake 💛" ]
+    
+   
+    
+}
